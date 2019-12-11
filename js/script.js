fdecffe8f56b6bb8890ebda73b9e0381de5a26f6
Agnieszka Dąbrowska <aeris@vp.pl>
2019-12-11 20:27:49 +0100

    Update content of script.js (computerMove)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem kamień! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+
