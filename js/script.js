42d0c98fb2fd1014ca79385555faf2e86f99f6a4
Agnieszka Dąbrowska <aeris@vp.pl>
2020-01-13 17:45:33 +0100

    Add cosmetic changes to code

--- js/script.js
+++ js/script.js
@@ -1,71 +1,62 @@
 function playGame(playerInput){
   clearMessages();
 
-
-function getMoveName(argMoveId){
-  if(argMoveId == 1){
-    return 'kamień';
-  } else if (argMoveId == 2){
-    return 'papier';
-  } else if (argMoveId == 3){
-    return 'nożyce';
-  } else {
-    printMessage('Nie znam ruchu: ' + argMoveId + '.');
-    return 'nieznany ruch';
+  function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    } else if (argMoveId == 2){
+      return 'papier';
+    } else if (argMoveId == 3){
+      return 'nożyce';
+    } else {
+      printMessage('Nie znam ruchu: ' + argMoveId + '.');
+      return 'nieznany ruch';
+    }
   }
-}
-
 
 /*
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 */
-let playerMove = getMoveName(playerInput);
 
-console.log('Ruch gracza: ' + playerMove);
+  let playerMove = getMoveName(playerInput);
+  console.log('Ruch gracza: ' + playerMove);
+  printMessage('Twój ruch to: ' + playerMove);
 
-printMessage('Twój ruch to: ' + playerMove);
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('Wylosowana liczba to: ' + randomNumber);
 
+  let computerMove = getMoveName(randomNumber);
+  console.log('Ruch komputera to: ' + computerMove);
+  printMessage('Mój ruch to: ' + computerMove);
 
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = getMoveName(randomNumber);
-console.log('Ruch komputera to: ' + computerMove);
-
-printMessage('Mój ruch to: ' + computerMove);
-
-
-
-function displayResult(argComputerMove, argPlayerMove){
-  console.log('Do displayResult wpadły: ' + argComputerMove + ' i ' + argPlayerMove);
-  if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+  function displayResult(argComputerMove, argPlayerMove){
+    console.log('Do displayResult wpadły: ' + argComputerMove + ' i ' + argPlayerMove);
+    if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+      return 'Ty wygrywasz!';
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+      return 'Ja wygrywam!';
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
     return 'Ty wygrywasz!';
-  } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
     return 'Ja wygrywam!';
-  } else if (argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
-  return 'Ty wygrywasz!';
-  } else if (argComputerMove == 'papier' && argPlayerMove == 'kamień') {
-  return 'Ja wygrywam!';
-  } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
-  return 'Ja wygrywam!';
-  } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
-  return 'Ty wygrywasz!';
-  } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch') {
-  return 'No co Ty?!';
-  } else if (argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch') {
-  return 'No co Ty?!';
-  } else if (argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch') {
-  return 'No co Ty?!';
-  } else {
-  return 'Remis!';
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'papier') {
+    return 'Ja wygrywam!';
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'kamień') {
+    return 'Ty wygrywasz!';
+    } else if (argComputerMove == 'nożyce' && argPlayerMove == 'nieznany ruch') {
+    return 'No co Ty?!';
+    } else if (argComputerMove == 'papier' && argPlayerMove == 'nieznany ruch') {
+    return 'No co Ty?!';
+    } else if (argComputerMove == 'kamień' && argPlayerMove == 'nieznany ruch') {
+    return 'No co Ty?!';
+    } else {
+    return 'Remis!';
+    }
   }
-}
 
-
-printMessage('Wynik to: ' + displayResult(computerMove, playerMove));
+  printMessage('Wynik to: ' + displayResult(computerMove, playerMove));
 }
 
 /*
